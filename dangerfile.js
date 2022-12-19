08464853c27357433ac4e0cc5799a2d91cd4de51
Clark, William <William.Clark@fmr.com>
2022-12-19 17:35:07 -0500

    Add coverage danger.js plugin.

--- dangerfile.js
+++ dangerfile.js
@@ -1,4 +1,9 @@
 import {message, danger} from "danger"
+import { codeCoverage } from "danger-plugin-code-coverage"
+
 
 const modifiedMD = danger.git.modified_files.join("- ")
 message("Changed Files in this PR: \n - " + modifiedMD)
+
+// Test code coverage of modified files.
+codeCoverage()
